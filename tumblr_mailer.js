4ae0e9da9e6dd1179f25f3a75bb9ac094a43cb1e
Jon Horn <jhorn85@gmail.com>
2015-10-27 16:25:11 -0400

    Got EJS template creating emails with proper data from the contact objects

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,7 +1,11 @@
 var fs = require('fs');
 var contacts = require("./contact_constructors.js");
+var ejs = require('ejs');
 var csvFile = fs.readFileSync("./friend_list.csv").toString();
 var emailHTML = fs.readFileSync('./email_template.html', 'utf-8');
+// var getTemplate = require("./email_template.ejs");
+// var renderedTemplate = ejs.render(emailHTML);
+//console.log(renderedTemplate);
 var parsedCSV = csvParse(csvFile);
 var contactArray = buildContactArray(parsedCSV);
 
@@ -19,15 +23,15 @@ function csvParse(file) {
 	var header = splitLines.shift().split(",");
 
 	// Create CSV Object with parsed contact info
-	var parsedCSV = new contacts.CSVObject(header, splitLines);
-	return parsedCSV;
+	var csv = new contacts.CSVObject(header, splitLines);
+	return csv;
 }
 
 /*
 Loops through a CSV Object and returns an array of objects
 */
 function buildContactArray(csvObject) {
-	var contactArray = [];
+	var newArray = [];
 
 	// Loop through the array of contacts
 	for (var i = 0; i < csvObject.arrayOfContacts.length; i++) {
@@ -39,15 +43,39 @@ function buildContactArray(csvObject) {
 			contact[csvObject.header[j]] = contactInfo[j];
 		};
 		// Add new contact to the contact array
-		contactArray.push(contact);
+		newArray.push(contact);
 	};
-	return contactArray;
+	return newArray;
+}
+
+function buildEmailEJS() {
+	
+	for (var i = 0; i < contactArray.length; i++) {
+		console.log(ejs.render(emailHTML,contactArray[i]));
+	};
+}
+
+function buildEmail_REPLACE_METHOD() {
+	var emailArray = [];
+	var firstName = /FIRST_NAME/gi;
+	var monthsSince = /NUM_MONTHS_SINCE_CONTACT/gi;
+
+	for (var i = 0; i < contactArray.length; i++) {
+		console.log();
+		console.log();
+		var newName = (emailHTML.replace(firstName, contactArray[i].firstName));
+		var newNameMonths = emailHTML.replace(monthsSince, contactArray[i].numMonthsSinceContact);
+		console.log(newNameMonths);
+		emailArray.push(newNameMonths);
+	};
 }
 
 // Main function
 function main() {
-	console.log(contactArray);
-	return contactArray;
+	//console.log(contactArray);
+	//buildEmail();
+	buildEmailEJS();
+	//return contactArray;
 }
 
 main();
